9cd67bcffb3b5f201bb18054ada62b58e9a6d21c
Henry <138160589+endvidous@users.noreply.github.com>
2023-12-21 23:46:46 +0530

    Load Images Change

--- src/Components/CarCard.jsx
+++ src/Components/CarCard.jsx
@@ -1,29 +1,59 @@
 import React, { useState, useEffect } from "react";
 import "../Css/index.css";
+import { useLocation } from "react-router-dom";
 
 const CarCard = ({ car }) => {
+  // const [imageExists, setImageExists] = useState(true);
+
+  // const location = useLocation();
+
+  // useEffect(() => {
+  //   const img = new Image();
+  //   img.src = car.Image;
+
+  //   // console.log("Attempting to load image", car.Image);
+
+  //   img.onload = () => {
+  //     // console.log("Image loaded successfully:", car.Image);
+  //     setImageExists(true);
+  //   };
+
+  //   img.onerror = () => {
+  //     // console.error("Error loading image:", car.Image);
+  //     setImageExists(false);
+  //   };
+  // }, [car.Image]);
+
+  // const imageSrc = imageExists
+  //   ? car.Image
+  //   : "https://cdn.shopify.com/s/files/1/0614/9947/4155/files/KITTEN-MEOW_480x480.jpg?v=1640061250";
+
   const [imageExists, setImageExists] = useState(true);
+  const location = useLocation();
 
   useEffect(() => {
     const img = new Image();
-    img.src = car.Image;
-
-    // console.log("Attempting to load image", car.Image);
+    img.src = getAbsolutePath(car.Image, location.pathname);
 
     img.onload = () => {
-      // console.log("Image loaded successfully:", car.Image);
       setImageExists(true);
     };
 
     img.onerror = () => {
-      // console.error("Error loading image:", car.Image);
       setImageExists(false);
     };
-  }, [car.Image]);
+  }, [car.Image, location.pathname]);
+
+  const getAbsolutePath = (relativePath, currentPath) => {
+    // Assuming your images are in the public folder
+    const publicPath = "/public";
+    return `${relativePath.replace(/^\./, '')}`;
+  };
 
   const imageSrc = imageExists
-    ? car.Image
+    ? getAbsolutePath(car.Image, location.pathname)
     : "https://cdn.shopify.com/s/files/1/0614/9947/4155/files/KITTEN-MEOW_480x480.jpg?v=1640061250";
+
 
   return (
     <div className="carCard">
